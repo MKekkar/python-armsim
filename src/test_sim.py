3f450274277540f04484879c713b0814f5ca3fe5
MKekkar <mkekkar@ucsd.edu>
2025-02-09 14:41:24 -0800

    update test_sim slightly

--- src/test_sim.py
+++ src/test_sim.py
@@ -4,13 +4,34 @@ if __name__ != '__main__':
     print('WARNING: this file is for testing purposes only and should NOT be imported anywhere')
     raise ImportError('Importing test-only module test_sim')
 
+is_even_instrs = '''
+is_even:
+    push {r4-r9, fp, lr}
+    add fp, sp, 28
+
+    and r0, r0, 0b1
+    eor r0, r0, 0x1
+
+    sub sp, fp, 28
+    pop {r4-r9, fp, lr}
+    bx lr
+'''
+
 func_instrs = '''
 func:
     push {r4-r9, fp, lr}
     add fp, sp, 28
 
+    mov r1, 2
+    sub sp, sp, 4
+    str r1, [sp]
+
     // return 0
     mov r0, 0
+
+    b .Lend: // skip to end (local label)
+    mov r0, 1
+.Lend:
 
     sub sp, fp, 28
     pop {r4-r9, fp, lr}
@@ -22,6 +43,14 @@ main:
     push {fp, lr}
     add fp, sp, 4
     bl func
+
+    mov r0, 0
+    mov r1, 1
+    mov r2, 2
+    mov r3, 3
+
+    bl is_even
+    .dump-reg r0
     sub sp, fp, 4
     pop {fp, lr}
     bx lr
@@ -29,6 +58,7 @@ main:
 dut = top_level()
 dut.add_file('main.S', main_instrs, is_main=True)
 dut.add_file('func.S', func_instrs)
+dut.add_file('is_even.S', is_even_instrs)
 print(dut.branch_table)
 print(dut.global_branch_table)
 print(dut.instrs)
@@ -43,7 +73,7 @@ while r and n_steps < MAX_STEPS:
     r = dut.step_instr()
     print(f"{dut.current_file}: {dut.rf.get('pc')}")
 formatted_regs = {key: hex(value) for (key,value) in dut.rf.regs.items()}
-formatted_mem = {key: hex(value) for (key,value) in dut.data.mem.items()}
+formatted_mem = dut.data.format()
 print(f'register values = {formatted_regs}')
-#print(f'data values = {formatted_mem}')
+print(f'data values =\n{formatted_mem}')
 
